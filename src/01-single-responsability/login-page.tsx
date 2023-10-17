d76320b0fad450f0ad431e28770db801194a4475
Juan Otálora Alarcón <19978031+juanoa@users.noreply.github.com>
2023-10-17 09:58:06 +0200

    01 - Login Page

---
+++ src/01-single-responsability/login-page.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+export const LoginPage = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleSubmit: React.FormEventHandler = (e) => {
+    e.preventDefault();
+    const errors = []
+    if (password.length < 5) {
+      errors.push("Password too short")
+    }
+    if (!password.match(/[!"#$%&'()*+,-.:;<=>?@[\]^_`{|}~]/)) {
+      errors.push("Password should contain a special character")
+    }
+
+    if (errors.length > 0) {
+      console.error(errors)
+    } else {
+      console.log(`Session started for email: ${email}`)
+    }
+  }
+
+  const onChangeEmail: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const onChangePassword: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setPassword(e.target.value)
+  }
+
+  return (
+    <div className="min-h-screen bg-amber-50 flex flex-col items-center justify-center">
+      <div className="bg-white flex flex-col items-center justify-center w-2/5 p-10 rounded-2xl gap-10">
+        <div className="flex flex-col content-center items-center gap-2">
+          <img src="https://picsum.photos/50/50" alt="Logo" />
+          <h1 className="text-4xl">Login</h1>
+        </div>
+        <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center gap-5">
+          <input
+            name="email"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={onChangeEmail}
+            className="border p-2 rounded"
+          />
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={onChangePassword}
+            className="border p-2 rounded"
+          />
+          <button type="submit" className="bg-amber-500 py-2 px-5 rounded">Log In</button>
+        </form>
+      </div>
+    </div>
+  )
+
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'
+import {Route} from "wouter";
+import {LoginPage} from "./01-single-responsability/login-page.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <h1>Hello World 🙂</h1>
+    <Route path="/1"><LoginPage /></Route>
   </React.StrictMode>,
 )

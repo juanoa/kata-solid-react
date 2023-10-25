df1535662e9fb6f431aab8eeed83ee645304d11d
Juan Otálora Alarcón <19978031+juanoa@users.noreply.github.com>
2023-10-25 14:08:25 +0200

    Main page

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import {Route} from "wouter";
+import {Link, Route} from "wouter";
 import {SingleResponsabilityPage} from "./01-single-responsability/single-responsability-page.tsx";
 import {InterfaceSegregationPage} from "./04-interface-segregation/interface-segregation-page.tsx";
 import {OpenClosedPage} from "./02-open-closed/open-closed-page.tsx";
@@ -11,5 +11,16 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Route path="/1"><SingleResponsabilityPage /></Route>
     <Route path="/2"><OpenClosedPage /></Route>
     <Route path="/4"><InterfaceSegregationPage /></Route>
+    <Route path="/">
+      <div className="w-screen h-screen flex items-center justify-center">
+        <div className="grid grid-cols-2 gap-5 text-center font-bold">
+          <Link className="px-10 py-5 bg-blue-100 rounded" to="/1">1 - Single Responsibility ⛑️</Link>
+          <Link className="px-10 py-5 bg-blue-100 rounded" to="/2">2 - Open-closed 🔓</Link>
+          <Link className="px-10 py-5 bg-blue-100 rounded" to="/3">3 - Liskov Substitution 🔨</Link>
+          <Link className="px-10 py-5 bg-blue-100 rounded" to="/4">4 - Interface Segregation ✂️</Link>
+          <Link className="px-10 py-5 bg-blue-100 rounded" to="/5">5 - Dependency Inversion 💉</Link>
+        </div>
+      </div>
+    </Route>
   </React.StrictMode>,
 )
